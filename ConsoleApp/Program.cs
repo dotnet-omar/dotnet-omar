ec8583572ed2c55a024731ed0afa49859f68bf05
Omar Elsayed <omar.alsayed@syscodeia.ae>
2023-03-24 23:15:41 +0200

    feat(console-app): add `console-app` to use libraries

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using DotnetOmar.Logger;
+
+TestLogger();
+
+
+static void TestLogger()
+{
+    var dictionary = new Dictionary<string, string>
+    {
+        { "name", "value" },
+        { "name1", "value" },
+        { "name2", "value" },
+        { "name3", "value" },
+    };
+
+    Logger.LogDictionary(dictionary);
+}
