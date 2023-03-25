a680defa5da3cb19dc89d9244a6cb0699926a297
Omar Elsayed <omar.alsayed@syscodeia.ae>
2023-03-25 04:30:56 +0200

    feat(helper): add dotnet helper library

---
+++ DotnetOmar.Helper/Helper.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace DotnetOmar.Helper;
+
+public static class Helper
+{
+    public static string GetHash(string text)
+    {
+        var textBytes = Encoding.UTF8.GetBytes(text);
+        var hash = SHA256.HashData(textBytes);
+        return BitConverter.ToString(hash);
+    }
+}
